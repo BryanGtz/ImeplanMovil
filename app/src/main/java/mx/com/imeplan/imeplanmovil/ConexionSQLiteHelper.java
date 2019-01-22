4adf60f7c8761abe3d3c0b94dd9bb13edd092d63
BryanGtzCastro <bryan.gtz.317@gmail.com>
2019-01-21 23:43:29 -0600

    Separado el código de las consultas a la BD a la clase correspondiente

--- app/src/main/java/mx/com/imeplan/imeplanmovil/ConexionSQLiteHelper.java
+++ app/src/main/java/mx/com/imeplan/imeplanmovil/ConexionSQLiteHelper.java
@@ -1,8 +1,11 @@
 package mx.com.imeplan.imeplanmovil;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 import mx.com.imeplan.imeplanmovil.utilidades.Utilidades;
 
@@ -28,6 +31,70 @@ public class ConexionSQLiteHelper extends SQLiteOpenHelper {
         db.execSQL("drop table if exists "+Utilidades.TABLA_SUBCATEGORIA);
         db.execSQL("drop table if exists "+Utilidades.TABLA_REPORTE);
         onCreate(db);
+    }
+
+    public String[] getCategorias(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor;
+        String[]categorias = {};
+        try {
+            //Select categoria from Categoria
+            cursor = db.rawQuery("select " + Utilidades.C_CAMPO_CATEGORIA +
+                    " from " + Utilidades.TABLA_CATEGORIA, null);
+            categorias = new String[cursor.getCount()+1];
+            categorias[0]="--Seleccione una categoria---";
+            int i = 0;
+            while(cursor.moveToNext()&&i<cursor.getCount()){
+                categorias[i+1] = cursor.getString(0);
+                i++;
+            }
+            db.close();
+            cursor.close();
+        }catch (Exception e){
+            Log.e("Error geting categories",e.getMessage());
+        }
+        return categorias;
+    }
+
+    public String[] getNombreSubcategorias (int idCategoria){
+        SQLiteDatabase db = getReadableDatabase();
+        //Select subcategoria from SubCategoria where categoria = idCategoria;
+        Cursor cursor = db.rawQuery("select "+Utilidades.SC_CAMPO_SUBCATEGORIA+
+                " from "+Utilidades.TABLA_SUBCATEGORIA +
+                " where "+Utilidades.SC_CAMPO_CATEGORIA+" = "+ idCategoria,null);
+        int count = cursor.getCount();
+        String [] subcategorias = new String[count];
+        int i = 0;
+        if(cursor.moveToFirst()){
+            subcategorias[i]=cursor.getString(0);
+            i++;
+            while (cursor.moveToNext()&&i<count) {
+                subcategorias[i] = cursor.getString(0);
+                i++;
+            }
+        }
+        db.close();
+        cursor.close();
+        return subcategorias;
+    }
+
+    public int getIdSubcategoria (String subnombre){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("select "+Utilidades.SC_CAMPO_ID+
+                        " from "+Utilidades.TABLA_SUBCATEGORIA +
+                        " where "+Utilidades.SC_CAMPO_SUBCATEGORIA+" like '"+subnombre+"'",null);
+        int num = cursor.getCount();
+        int [] subcategorias = new int[num];
+        int i = 0;
+        if(cursor.moveToFirst()){
+            while (cursor.moveToNext()&&i<num) {
+                subcategorias[i] = cursor.getInt(0);
+                i++;
+            }
+        }
+        db.close();
+        cursor.close();
+        return  subcategorias[0];
     }
 
 }
